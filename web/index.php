edfebf5e9fca33b61c5f4af9b2a4d1007b7cb5a7
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-11-19 15:21:58 +0100

    Update:(index) - add "controller.php"

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@ session_start();
 // require core
 require_once 'application/core/config.php';
 require_once 'application/core/app.php';
+require_once 'application/core/controller.php';
 $app = new App;
 
 ?>
