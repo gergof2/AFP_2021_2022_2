7d8b5fc72437431cb7dc68ba166bd6665582d6f3
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2021-11-19 15:10:06 +0100

    Update:(controller) -add function "load_model"

--- web/application/core/controller.php
+++ web/application/core/controller.php
@@ -5,4 +5,9 @@ class Controller {
     public function __construct() {
         
     }
+
+    public function load_model($model) {
+        require_once 'application/models/'.$model.'.php';
+        return new $model;
+    }
 }
