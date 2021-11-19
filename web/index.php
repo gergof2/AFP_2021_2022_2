6277d7c8c5f4bdec494dc85b5c0aaae7cd2c7dd0
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-11-19 15:22:10 +0100

    Update:(index) - add "model.php"

--- web/index.php
+++ web/index.php
@@ -6,6 +6,8 @@ session_start();
 require_once 'application/core/config.php';
 require_once 'application/core/app.php';
 require_once 'application/core/controller.php';
+require_once 'application/core/model.php';
+
 $app = new App;
 
 ?>
