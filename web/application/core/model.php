2857e8245c4c01b17b6dab6d1d5481000d17b29a
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-11-19 14:51:50 +0100

    Update:(model) - add "getList"

--- web/application/core/model.php
+++ web/application/core/model.php
@@ -6,4 +6,14 @@ class Model {
         $connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
         return $connection;
     }
+
+    public function getList($queryString, $queryParams = []) {
+        $connection = $this->getConnection();  
+        $statement = $connection->prepare($queryString);
+        $success = $statement->execute($queryParams);
+        $result = $success ? $statement->fetchAll() : $statement->errorInfo()[2];
+        $statement->closeCursor();
+        $connection = null;
+        return $result;
+    }
 }
