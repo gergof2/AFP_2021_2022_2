e06869d216e4eeacadfddc8bd5469049ff4c292c
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-11-19 14:50:43 +0100

    Update:(model) - add "getConnection"

--- web/application/core/model.php
+++ web/application/core/model.php
@@ -1,4 +1,9 @@
 <?php
 
 class Model {
+
+    public function getConnection() {
+        $connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
+        return $connection;
+    }
 }
