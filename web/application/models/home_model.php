dbb6e017774e28cebd024e7fb8889a2cc8448752
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-11-26 14:20:53 +0100

    Update:(home_model) - add "getMessages"

--- web/application/models/home_model.php
+++ web/application/models/home_model.php
@@ -1,4 +1,10 @@
 <?php
 
 class home_model extends Model {
+
+    public function getMessages() {
+        $messages = $this->getList("SELECT * FROM `messages`");
+
+        return $messages;
+    }
 }
