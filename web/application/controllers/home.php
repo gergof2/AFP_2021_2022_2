18dfa3daea722da210c24a0f153a51676cba14f5
Seres Péter <71933391+SeresP99@users.noreply.github.com>
2021-11-26 14:51:07 +0100

    Update:(home) - add constructor

--- web/application/controllers/home.php
+++ web/application/controllers/home.php
@@ -4,4 +4,12 @@ class Home extends Controller {
 
     public $model;
    
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->model = $this->load_model('api_model');
+
+
+    }
 }
