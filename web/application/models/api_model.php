c5efaf3fbcdbb6343d7ce23de8436987d8d9fc8b
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-11-26 14:22:14 +0100

    Update:(api_model) - add "getLogin"

--- web/application/models/api_model.php
+++ web/application/models/api_model.php
@@ -1,3 +1,12 @@
 <?php
 class api_model extends Model {
+
+	public function getLogin($username, $password) {
+        $query = "SELECT * FROM user WHERE username = :username AND password = :password";
+        $query_params = array(         
+            ':username' => $username,
+            ':password' => sha1($password)
+        );        
+        return $this->getRecord($query, $query_params);       
+    }
 }
