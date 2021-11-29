966650d5784e2c7c81a6f55a4af31c3245c10fa3
Szalóki Dávid <Szunyix@gmail.com>
2021-11-29 19:25:23 +0100

    Add : (frontend) header bootstrap addon

--- web/application/views/template/header.php
+++ web/application/views/template/header.php
@@ -5,4 +5,19 @@
 <html lang="en" data-theme="<?=$_SESSION['data-theme']?>">
 
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?=SITE_NAME?></title>
+
+    <!-- Bootstrap, fontawesome, CSS -->   
+    <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <!-- URL LINK : <?=URL?>/assets/css/style.css -->
+    <link rel="stylesheet" href="/assets/vendor/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!-- URL LINK : <?=URL?>/assets/vendor/fontawesome/css/all.min.css -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+    
+</head>
 </header>
