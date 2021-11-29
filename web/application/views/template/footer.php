eb19716ec904ab4c7e7d096f5e45091be6b40897
Szalóki Dávid <Szunyix@gmail.com>
2021-11-29 19:07:53 +0100

    Add: (frontend) -footer Help menu

---
+++ web/application/views/template/footer.php
@@ -0,0 +1,21 @@
+</body>
+<footer class="footer">
+	<div class="footer-left"> <!-- first element -->
+		<img src="/assets/vendor/images/large.png">	<!-- picture -->	
+	</div>
+	<ul class="footer-right">
+		<li>  <!-- Second element -->
+			<h2>Need Help?</h2>
+			<ul class="box">
+				<li><a href="/Howtoregister">How to register</a></li>	
+				<li><a href="#">Brouchures</a></li>
+				<li><a href="#">Donate</a></li>
+				<li><a href="#">Grovernace</a></li>
+				<li><a href="#">Impact Report</a></li>				
+			</ul>
+</footer>
+
+
+
+</html>
+
