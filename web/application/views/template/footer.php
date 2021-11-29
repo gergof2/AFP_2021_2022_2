122ba186f4f661f61a0df058f0cdbc67121d6789
Szalóki Dávid <Szunyix@gmail.com>
2021-11-29 19:08:21 +0100

    Add: (frontend) -footer features menu

--- web/application/views/template/footer.php
+++ web/application/views/template/footer.php
@@ -13,6 +13,15 @@
 				<li><a href="#">Grovernace</a></li>
 				<li><a href="#">Impact Report</a></li>				
 			</ul>
+		</li> <!-- third element -->
+		<li class="features"> 
+			<h2>Follow Us</h2>
+			<ul class="box">
+				<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</li>
 </footer>
 
 
